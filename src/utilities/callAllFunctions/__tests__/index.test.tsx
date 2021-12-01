ef02d167af3a46e9edf5a0f81de2697b90d0f2b7
Louis Young <me@louisyoung.co.uk>
2021-12-01 22:56:19 +0000

    Update unit tests

--- src/utilities/callAllFunctions/__tests__/index.test.tsx
+++ src/utilities/callAllFunctions/__tests__/index.test.tsx
@@ -1,23 +1,19 @@
-import { waitFor } from "@testing-library/react";
 import { callAllFunctions } from "..";
 
-/**
- * This test requires further investigation as it is failing.
- */
+describe("callAllFunctions", () => {
+  it("calls all functions with arguments", async () => {
+    const firstArgument = "__FIRST_ARGUMENT__";
+    const secondArgument = "__SECOND_ARGUMENT__";
 
-describe.skip("callAllFunctions", () => {
-  it("calls all functions", async () => {
     const firstFunction = jest.fn();
     const secondFunction = jest.fn();
 
-    callAllFunctions(firstFunction, secondFunction);
+    callAllFunctions(firstFunction, secondFunction)(
+      firstArgument,
+      secondArgument,
+    );
 
-    await waitFor(() => {
-      expect(firstFunction).toHaveBeenCalledTimes(1);
-      expect(secondFunction).toHaveBeenCalledTimes(1);
-    });
-
-    expect(firstFunction).toHaveBeenCalledTimes(1);
-    expect(secondFunction).toHaveBeenCalledTimes(1);
+    expect(firstFunction).toHaveBeenCalledWith(firstArgument, secondArgument);
+    expect(secondFunction).toHaveBeenCalledWith(firstArgument, secondArgument);
   });
 });
